71d0809f851b43a0eed6a36563231a8a1c37968c
wangxinran@iwgame.com <wangxinran@iwgame.com>
2021-01-22 15:14:20 +0800

    delete sometings

--- tests/test_detect.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from test import Chrome, ChromeOptions
-
-options = ChromeOptions()
-chrome = Chrome(options=options)
-
-chrome.get(url="https://bot.sannysoft.com/")
-import pdb;pdb.set_trace()
